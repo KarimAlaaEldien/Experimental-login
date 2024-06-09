dab7f7c0f79397395f5556e9c68b2903743c893a
KarimAlaaEldien <karimalaa845@gmail.com>
2024-06-09 05:35:00 +0300

    first commit

---
+++ home/js/home.js
@@ -0,0 +1,14 @@
+var getName=JSON.parse(localStorage.getItem("userName"));
+var welcomeMessage=document.querySelector("#welcomeMessage");
+var logout=document.querySelector("#logout");
+
+welcomeMessage.innerHTML=`Welcome ${getName}`;
+
+logout.addEventListener("click",function(){
+    localStorage.removeItem("userName")
+    location="../../login/login.html"
+})
+
+
+
+
